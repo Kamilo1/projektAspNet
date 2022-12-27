c013053fe84f965516fa538236f89458c3249760
Tomasz <Tomasz@MSI-TOMASZ>
2022-12-27 18:33:42 +0100

    Dodaj pliki projektów.

---
+++ projektAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projektAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

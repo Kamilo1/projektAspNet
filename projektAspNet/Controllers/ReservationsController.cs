2a6218749e2b8a76d7e3bdd231fd2b27651e8eb1
Kamil Panuś <Kamil.Panus@microsoft.wsei.edu.pl>
2023-01-07 15:23:23 +0100

    kontroler rezerwacji z metoda index

---
+++ projektAspNet/Controllers/ReservationsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projektAspNet.Models;
+
+namespace projektAspNet.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ReservationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: ReservationsController
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Reservations.ToListAsync());
+        }
+
+        // GET: ReservationsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservationsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RouteID,CustomerID,KayakType,NumberOfKayak")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: ReservationsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

08ef820c0ce09ccf76e4b80656d6c181a991c06f
Kamil Panuś <Kamil.Panus@microsoft.wsei.edu.pl>
2023-01-23 21:08:28 +0100

    users

---
+++ projektAspNet/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace projektAspNet.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string? Name { get; set; }
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Surname { get; set; }
+}
+
--- projektAspNet/Program.cs
+++ projektAspNet/Program.cs
@@ -3,12 +3,23 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using projektAspNet.Service;
+using projektAspNet.Data;
+using projektAspNet.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEventsService, EventsServiceEF>();
 builder.Services.AddScoped<ICustomersService, CustomersServiceEF>();
 builder.Services.AddScoped<IRoutesService, RoutesServiceEF>();
@@ -25,12 +36,13 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthorization();
-
+app.UseSession();
+app.UseAuthentication();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+public partial class Program { }
